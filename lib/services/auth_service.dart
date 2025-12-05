85a8fd1f18a5ee905bda4d2ffc32e88cd5e7136e
ranggadya <ranggadya2530@gmail.com>
2025-12-05 15:29:41 +0700

    first commit

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,44 @@
+  import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return result.user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future<User?> signUp(String email, String password, String username) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      
+      if (user != null) {
+        await user.updateDisplayName(username);
+        await user.reload();
+
+        await _firestore.collection('users').doc(user.uid).set({
+          'uid': user.uid,
+          'username': username,
+          'email': email,
+        });
+      }
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Logout
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
